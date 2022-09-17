5e04240a40b5de2ea9a481bbfc571493ad048f9d
Fahad123k <ahmad.nishu08@gmail.com>
2022-09-17 19:10:49 +0530

    change time format

--- script/script.js
+++ script/script.js
@@ -136,15 +136,15 @@ setAlarm.addEventListener("click", e => {
     if (h > 12) {
       h = h - 12;
     }
-    if (h < 10) {
-      h = '0' + h;
-    }
-    if (m < 10) {
-      m = '0' + m;
-    }
-    if (s < 10) {
-      s = '0' + s;
-    }
+    // if (h < 10) {
+    //   h = '0' + h;
+    // }
+    // if (m < 10) {
+    //   m = '0' + m;
+    // }
+    // if (s < 10) {
+    //   s = '0' + s;
+    // }
 
 
     // alert("alarm set at-" + h + " " + m + " " + s + " " + amPm);
