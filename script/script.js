bfeeec6ac8ab233582edb9027c052029b18741ad
Fahad123k <ahmad.nishu08@gmail.com>
2022-09-25 11:39:35 +0530

    prev

--- script/script.js
+++ script/script.js
@@ -138,17 +138,17 @@ setAlarm.addEventListener("click", e => {
 
     if (h > 12) {
       h = h - 12;
+      // h='0'+h
     }
 
     if (amPm != "AM") {
       if (h < 10) {
         h = '0' + h;
       }
-      if(h==12){
-        h=00;
-      }
+     
     }
    
+  
     // if (m < 10) {
     //   m = '0' + m;
     // }
